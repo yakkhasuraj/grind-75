579290e26cd39ab49c9ec15b4d50870b4d2828aa
Suraj Yakkha <yakkhasuraj@gmail.com>
2025-01-16 19:00:59 +0545

    feat: implement two sum solution

---
+++ 01-two-sum/index.js
@@ -0,0 +1,29 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
+// O(n) time complexity and O(n) space complexity
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+nums = [3, 2, 4];
+target = 6;
+console.log(twoSum(nums, target));
